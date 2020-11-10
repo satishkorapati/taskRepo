77977cf407ad72aa554766f7cb36d49aff7cfb2e
satishkorapati <satishkorapati@gmail.com>
2020-11-10 02:57:40 +0000

    Added Multiply Method

--- maths.py
+++ maths.py
@@ -1,12 +1,19 @@
 
-
-
-
 def add(a,b):
-	c =a+b
-	print("Sum of 2 number: "+str(c)
+	c=a+b
+	print("Sum of 2 numbers: "+str(c))
 
 def sub(a,b):
-	c =a-b
-	print("Difference Between 2 numbers is: "+str(c))
+	c=a-b
+	print("Difference of 2 numbers: "+str(c))
+
+def mutliply(a,b):
+	c=a*b
+	print("Product of 2 numbers: "+str(c)
+
+
+
+
+
+
 
