4d9265fd21b951301a5d56367a3b08ff40796f73
satishkorapati <satishkorapati@gmail.com>
2020-11-11 18:51:52 +0530

    Update hello.py
    Pull Changes use case

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@
 print("This is my first repository")
 print("Checking for the Task Repo Project by using Webhook")
 print("This line is added from the EC2 to check the commit from EC2")
+print("To check the Pull")
 
 
 
